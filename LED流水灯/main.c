a3351e1a2fed4d3eeb88e82ae8c2554ee05b5427
NashWang1997 <34232770+NashWang1997@users.noreply.github.com>
2018-01-23 20:58:08 +0800

    Add files via upload

---
+++ LED流水灯/main.c
@@ -0,0 +1,28 @@
+#include "reg51.h"
+#include "intrins.h"
+
+typedef unsigned char u8;
+typedef unsigned int u16;
+
+#define led P0
+
+void delay(u16 i)
+{
+	while (i--);
+}
+void main()
+{
+	u8 i;
+	led = 0x01;
+	delay(50000);
+	while (1) {
+		for (i=0; i<7; ++i) {
+			led = _crol_(led, 1);
+			delay(50000);
+		}
+		for (i=0; i<7; ++i) {
+			led = _cror_(led, 1);
+			delay(50000);
+		}
+	}
+}
